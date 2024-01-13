f41d2e8b14625a412bcfd50d59d0b52d49925cbc
unknown <toolvlg@gmail.com>
2024-01-13 18:48:49 +0400

    create summarize route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import express from "express";
 import StartWithAIRoute from "./routes/StartWithAI.js";
 import ExpandWithAIRoute from "./routes/ExpandWithAI.js";
+import SummarizeWithAIRoute from "./routes/SummarizeWithAI.js";
 
 const app = express();
 app.use(cors());
@@ -16,6 +17,7 @@ app.use(express.json());
 // Use the chat route
 app.use("/startwithai", StartWithAIRoute);
 app.use("/expandwithai", ExpandWithAIRoute);
+app.use("/summarizewithai", SummarizeWithAIRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
